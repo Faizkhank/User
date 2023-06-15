071dfdae42a6f4fbe5c30877661034859a25a5ea
faiz khan <fine7production@gmail.com>
2023-06-15 21:30:58 +0530

    commit

---
+++ src/Component/Home.jsx
@@ -0,0 +1,257 @@
+import { React, useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  const [age, setage] = useState();
+  const [response, setresponse] = useState();
+  const [Data, setData] = useState([]);
+  const [countries, setCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [selectedState, setSelectedState] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
+  const [userdata, setusedata] = useState({});
+  console.log(response);
+  useEffect(() => {
+    axios
+      .get(
+        "https://pkgstore.datahub.io/core/world-cities/world-cities_json/data/5b3dd46ad10990bca47b04b4739a02ba/world-cities_json.json"
+      )
+      .then((res) => {
+        const uniqueCountries = [
+          ...new Set(res.data.map((city) => city.country)),
+        ];
+        setCountries(uniqueCountries);
+        setData(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  const filteredStates = selectedCountry
+    ? [
+        ...new Set(
+          Data.filter((city) => city.country === selectedCountry).map(
+            (city) => city.subcountry
+          )
+        ),
+      ]
+    : [];
+
+  const filteredCities = selectedState
+    ? [
+        ...new Set(
+          Data.filter(
+            (city) =>
+              city.country === selectedCountry &&
+              city.subcountry === selectedState
+          ).map((city) => city.name)
+        ),
+      ]
+    : [];
+  const handleCountryChange = (e) => {
+    const country = e.target.value;
+    setusedata({ ...userdata, country: country });
+    setSelectedCountry(country);
+    setSelectedState("");
+    setSelectedCity("");
+  };
+
+  const handleStateChange = (e) => {
+    const state = e.target.value;
+    setusedata({ ...userdata, state: state });
+    setSelectedState(state);
+    setSelectedCity("");
+  };
+
+  const handleInput = (e) => {
+    setusedata({ ...userdata, [e.target.name]: e.target.value });
+  };
+  const handleAge = async (dateOfBirth) => {
+    var today = new Date();
+    var userbirthDate = new Date(dateOfBirth);
+
+    var userage = today.getFullYear() - userbirthDate.getFullYear();
+    var monthDiff = today.getMonth() - userbirthDate.getMonth();
+    if (
+      monthDiff < 0 ||
+      (monthDiff === 0 && today.getDate() < userbirthDate.getDate())
+    ) {
+      userage--;
+    }
+    setage(userage);
+    setusedata({ ...userdata, age: userage, dateOfBirth: dateOfBirth });
+  };
+  const handleSubmit = async () => {
+    console.log(userdata);
+    if (age > 14) {
+      axios
+        .post("http://localhost:4000/api-user/add", userdata)
+        .then((res) => {
+          setresponse(res.data);
+        })
+        .catch((err) => console.log(err));
+    } else {
+      alert("user is under 14 are not allowed");
+    }
+  };
+  const handleCityChange = (e) => {
+    const city = e.target.value;
+    setusedata({ ...userdata, city: city });
+
+    setSelectedCity(city);
+  };
+  return (
+    <div className="flex justify-center  items-center h-screen">
+      <div className="w-[500px] h-auto font-Rampart">
+        <div className="w-full bg-slate-200 p-2 rounded-lg h-full  shadow-lg">
+          <label className="font-semibold text-md text-slate-500 py-3">
+            First Name:
+            <input
+              type="text"
+              name="first_name"
+              className="w-full rounded-lg h-12 p-3"
+              onChange={handleInput}
+            />
+          </label>
+
+          <label className="font-semibold text-md text-slate-500 py-3">
+            Last Name:
+            <input
+              type="text"
+              className="w-full rounded-lg h-12 p-3"
+              name="last_name"
+              onChange={handleInput}
+            />
+          </label>
+
+          <label className="font-semibold text-md text-slate-500 py-3">
+            Email:
+            <input
+              type="email"
+              className="w-full rounded-lg h-12 p-3"
+              name="email"
+              onChange={handleInput}
+            />
+          </label>
+          <div className="p-3">
+            <label className="mb-4">
+              Country:
+              <select value={selectedCountry} onChange={handleCountryChange}>
+                <option value="">Select a country</option>
+                {countries.map((country, index) => (
+                  <option key={index} value={country}>
+                    {country}
+                  </option>
+                ))}
+              </select>
+            </label>
+
+            {selectedCountry && (
+              <label className="mt-2">
+                State:
+                <select value={selectedState} onChange={handleStateChange}>
+                  <option value="">Select a state</option>
+                  {filteredStates.map((state, index) => (
+                    <option key={index} value={state}>
+                      {state}
+                    </option>
+                  ))}
+                </select>
+              </label>
+            )}
+
+            {selectedState && (
+              <label>
+                City:
+                <select value={selectedCity} onChange={handleCityChange}>
+                  <option value="">Select a city</option>
+                  {filteredCities.map((city, index) => (
+                    <option key={index} value={city}>
+                      {city}
+                    </option>
+                  ))}
+                </select>
+              </label>
+            )}
+          </div>
+          <div className="font-semibold text-md text-slate-500 flex">
+            <h1>Gender</h1>
+            <div className="my-5 rounded-lg flex">
+              <label className="p-1">
+                <input
+                  type="radio"
+                  name="gender"
+                  value="Male"
+                  onChange={handleInput}
+                />
+                Male
+              </label>
+              <br />
+              <label className="p-1">
+                <input
+                  type="radio"
+                  name="gender"
+                  value="Female"
+                  onChange={handleInput}
+                />
+                Female
+              </label>
+              <br />
+              <label className="p-1">
+                <input
+                  type="radio"
+                  name="gender"
+                  value="Other"
+                  onChange={handleInput}
+                />
+                Other
+              </label>
+            </div>
+          </div>
+          <div className="flex w-full justify-around">
+            <label className="w-[180px] px-2 ">
+              Date of Birth:
+              <input
+                type="date"
+                className="w-full rounded-lg h-12 p-3"
+                name="dateOfBirth"
+                onChange={(e) => {
+                  handleInput(e);
+                  handleAge(e.target.value);
+                }}
+              />
+            </label>
+
+            <label className="w-[70px]">
+              Age:
+              {age ? (
+                <p className="bg-white rounded-lg py-3 text-center">{age}</p>
+              ) : null}
+            </label>
+          </div>
+          <div className=" flex justify-center">
+            <button
+              onClick={handleSubmit}
+              className="py-2 px-4 mt-2 bg-black text-white rounded-2xl"
+              type="submit"
+            >
+              Add User
+            </button>
+          </div>
+        </div>
+        {response ? (
+          <div className="w-full h-[70px] rounded-lg shadow-lg p-3">
+            <p className=" font-semibold">User database link</p>
+            <Link
+              to={`http://localhost:3000/user/${response.userId}`}
+              className=" text-blue-400"
+            >
+              http://localhost:3000/user/{response.userId}
+            </Link>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Component/User.jsx
@@ -0,0 +1,54 @@
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+const User = () => {
+  const param = useParams();
+  const [user, setuser] = useState("");
+  console.log(user);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/api-user/${param.id}`)
+      .then((res) => {
+        if (res.data.success === true) {
+          setuser(res.data.users);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <div className="flex justify-center  items-center h-screen">
+      <div className="w-[400px] h-[400px] bg-slate-100 rounded-xl font-Rampart">
+        <p className="font-semibold text-xl text-center">User details</p>
+        <div className="p-6">
+          <p className="font-semibold">
+            Name: <span>{user.firstName}</span>
+          </p>
+          <p className="font-semibold">
+            Email: <span>{user.email}</span>
+          </p>
+          <p className="font-semibold">
+            Gender: <span>{user.gender}</span>
+          </p>
+          <p className="font-semibold">
+            Country: <span>{user.country}</span>
+          </p>
+          <p className="font-semibold">
+            State: <span>{user.state}</span>
+          </p>
+          <p className="font-semibold">
+            City: <span>{user.city}</span>
+          </p>
+          <p className="font-semibold">
+            Age: <span>{user.age}</span>
+          </p>
+          <p className="font-semibold">
+            Dateofbirth: <span>{user?.dateOfBirth?.substring(0, 16)}</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default User;
